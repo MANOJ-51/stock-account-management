e220e78f910e743da83acf3631cce8429ae4fd09
MANOJ-51 <m4anoj51@gmail.com>
2022-06-13 23:41:25 +0530

    added :credit and debit method

--- com/stock/account/management/Stocks.java
+++ com/stock/account/management/Stocks.java
@@ -10,12 +10,15 @@ public class Stocks {
 
     //initializing array list
     static ArrayList<StockPortfolio> portfolioArrayList = new ArrayList<StockPortfolio>();
+    static int accountBalance = 0; //uc2 variable for account balance reference
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO STOCK ACCOUNT MANAGEMENT");
         Stocks object = new Stocks();
         object.addNumberOfStocks();
         object.displayTotalValueOfEachStock();
+        object.credit();
+        object.debit();
     }
 
     //adding stocks method
@@ -64,5 +67,27 @@ public class Stocks {
             System.out.println(stockPortfolio.toString());
         }
         printTotalValueOfStock();
+    }
+
+    //uc2 credit method
+    public void credit() {
+        System.out.println("Enter the amount to credit in account");
+        int amountToBeCridited = scanner.nextInt();
+
+        accountBalance += amountToBeCridited;
+        System.out.println("Your account balance is :- " + accountBalance);
+    }
+
+    //uc2 debit method
+    public void debit() {
+        System.out.println("Enter the amount to be debited ");
+        int amountToBeDebited = scanner.nextInt();
+
+        if (amountToBeDebited <= accountBalance) {
+            accountBalance -= amountToBeDebited;
+            System.out.println("Your amount debited sucessfully");
+        } else {
+            System.out.println("Insufficient balance");
+        }
     }
 }
