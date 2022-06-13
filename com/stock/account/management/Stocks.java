4f56807352b3373a6c46269356ecf7982f620e56
MANOJ-51 <m4anoj51@gmail.com>
2022-06-13 22:10:43 +0530

    displaying welcome message

---
+++ com/stock/account/management/Stocks.java
@@ -0,0 +1,7 @@
+package com.stock.account.management;
+
+public class Stocks {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO STOCK ACCOUNT MANAGEMENT");
+    }
+}
